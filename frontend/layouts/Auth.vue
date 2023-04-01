d5ec0adb536da91fbd049294198a3d6bf40a121e
buzzy844 <buzzy74@yahoo.com>
2023-04-01 23:03:05 +0300

    loading login done

--- frontend/layouts/Auth.vue
+++ frontend/layouts/Auth.vue
@@ -2,8 +2,8 @@
   <div class="row items-center justify-center bg-primary fullscreen">
     <div class="col-xs-12 col-lg-4">
       <div class="row flex-center text-center q-py-md">
-        <div class="row items-center no-pointer-events non-selectable">
-          <span>
+        <div class="row flex-center no-pointer-events non-selectable">
+          <span class="col-xs-12 col-sm-shrink">
             <q-icon
               color="secondary"
               name="apartment"
@@ -23,7 +23,7 @@
     </div>
 
     <div class="q-pa-md col-xs-12 col-lg-3">
-      <loading-login v-if="userSaved" />
+      <loading-login v-if="true" />
 
       <router-view v-else />
     </div>
--- frontend/pages/LoadingLogin.vue
+++ frontend/pages/LoadingLogin.vue
@@ -1,8 +1,9 @@
 <template>
   <q-card bordered flat>
     <q-card-section>
-      <div class="col-12 text-h6 text-center">
-        Acum te loghezi asteapta putin !!!
+      <div class="text-h6 text-center row flex-center q-col-gutter-md">
+        <div><q-spinner-pie color="primary" size="2em" class="" /></div>
+        <div>Hold on ,We're logging you in!</div>
       </div>
     </q-card-section>
   </q-card>
